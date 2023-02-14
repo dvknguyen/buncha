20ed03bf13ea04cbe72f7936d0221db8b5a1768d
Duy <duy@pop-os.localdomain>
2023-02-15 00:28:39 +0100

    navbar responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdRestaurantMenu } from "react-icons/md";
+import { GiHamburgerMenu, GiChopsticks } from "react-icons/gi";
 import "./Navbar.css";
 import images from "../../constants/images";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -33,6 +33,49 @@ export default function Navbar() {
           </a>
         </li>
       </ul>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#000000"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        >
+          {" "}
+        </GiHamburgerMenu>
+
+        {toggleMenu && (
+          <div className="app_navbar-smallscreen_overlay flex__center slide_botton">
+            <GiChopsticks
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            ></GiChopsticks>
+            <ul className="app__navbar-smallscreen_links">
+              <li>
+                <a href="#home" className="a-font">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#menu" className="a-font">
+                  Menü
+                </a>
+              </li>
+              <li c>
+                <a href="#contact" className="a-font">
+                  Kontakt
+                </a>
+              </li>
+              <li>
+                <a href="#resevation" className="a-font">
+                  Resevierung
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
